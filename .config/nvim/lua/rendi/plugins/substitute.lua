a096cc248b91f3df2d6ae3b64bafb3e5dd864b01
rendi12345678 <hardleberg@gmail.com>
2025-03-31 03:29:05 +0700

    update

--- .config/nvim/lua/rendi/plugins/substitute.lua
+++ .config/nvim/lua/rendi/plugins/substitute.lua
@@ -7,11 +7,11 @@ return {
     substitute.setup()
 
     -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap
 
     keymap.set("n", "<leader>s", substitute.operator, { desc = "Substitute with motion" }) -- Use <leader>s instead of s
-    keymap.set("n", "<leader>ss", substitute.line, { desc = "Substitute line" })          -- Use <leader>ss instead of ss
-    keymap.set("n", "<leader>S", substitute.eol, { desc = "Substitute to end of line" })   -- Use <leader>S instead of S
+    keymap.set("n", "<leader>ss", substitute.line, { desc = "Substitute line" }) -- Use <leader>ss instead of ss
+    keymap.set("n", "<leader>S", substitute.eol, { desc = "Substitute to end of line" }) -- Use <leader>S instead of S
     keymap.set("x", "<leader>s", substitute.visual, { desc = "Substitute in visual mode" }) -- Use <leader>s in visual mode instead of s
   end,
 }
