88946c7416a121b40b4f07a86472fc5db949b5d8
rendi12345678 <hardleberg@gmail.com>
2025-04-16 22:04:43 +0700

    update

--- .config/nvim/lua/rendi/plugins/leap.lua
+++ .config/nvim/lua/rendi/plugins/leap.lua
@@ -2,12 +2,6 @@ return {
   "ggandor/leap.nvim",
   config = function()
     local leap = require("leap")
-
     leap.set_default_keymaps()
-
-    -- Add custom mappings with the `z` prefix to avoid conflicts
-    -- vim.keymap.set({'n', 'x', 'o'}, 's', function() leap.leap({}) end, {desc = "Leap forward"})
-    -- vim.keymap.set({'n', 'x', 'o'}, 'S', function() leap.leap({backward = true}) end, {desc = "Leap backward"})
-    -- vim.keymap.set({'n', 'x', 'o'}, 'zx', '<Plug>(leap-from-window)')
   end,
 }
