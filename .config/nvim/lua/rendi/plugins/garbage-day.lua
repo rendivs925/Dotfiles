b11277ecf701b046d58027fee34a82027567c51e
rendi12345678 <hardleberg@gmail.com>
2025-04-27 23:05:41 +0700

    update

--- .config/nvim/lua/rendi/plugins/garbage-day.lua
+++ .config/nvim/lua/rendi/plugins/garbage-day.lua
@@ -7,15 +7,6 @@ return {
     excluded_lsp_clients = { "rust_analyzer" },
     grace_period = 60 * 15,
   },
-  config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.rust_analyzer.setup({
-      on_attach = function(client, bufnr)
-        if vim.api.nvim_buf_is_loaded(bufnr) then
-        end
-      end,
-    })
-  end,
   debug = {
     notifications = true,
     retries = 3,
