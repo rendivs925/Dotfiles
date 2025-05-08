e50b96b8cd15c5661ca8eb6934541c4ccf2a4859
rendi12345678 <hardleberg@gmail.com>
2025-05-08 20:32:34 +0700

    update

--- .config/nvim/lua/rendi/plugins/mason.lua
+++ .config/nvim/lua/rendi/plugins/mason.lua
@@ -1,20 +1,16 @@
 return {
   "williamboman/mason.nvim",
-  cmd = "Mason",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    -- import mason
     local mason = require("mason")
 
-    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
 
     local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
     mason.setup({
       ui = {
         icons = {
@@ -26,7 +22,6 @@ return {
     })
 
     mason_lspconfig.setup({
-      -- list of servers for mason to install
       ensure_installed = {
         "ts_ls",
         "html",
@@ -45,10 +40,10 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
+        "prettier",
+        "stylua",
+        "isort",
+        "black",
         "pylint",
         "asmfmt",
         "xmlformatter",
