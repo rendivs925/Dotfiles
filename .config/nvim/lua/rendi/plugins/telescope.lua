661629292f278576380298a75ba7b0de8898fa1a
rendi12345678 <hardleberg@gmail.com>
2025-07-10 10:00:52 +0700

    add todo telescope

--- .config/nvim/lua/rendi/plugins/telescope.lua
+++ .config/nvim/lua/rendi/plugins/telescope.lua
@@ -7,19 +7,41 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "LukasPietzschmann/telescope-tabs",
+    {
+      "folke/todo-comments.nvim",
+      opts = {}, -- add this line to trigger setup()
+    },
   },
   config = function()
     local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local transform_mod = require("telescope.actions.mt").transform_mod
+
+    local trouble = require("trouble")
+    local trouble_telescope = require("trouble.sources.telescope")
+
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function(prompt_bufnr)
+        trouble.toggle("quickfix")
+      end,
+    })
 
     telescope.setup({
       defaults = {
         path_display = { "smart" },
         file_ignore_patterns = { "node_modules" },
+        mappings = {
+          i = {
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<C-t>"] = trouble_telescope.open,
+          },
+        },
       },
     })
 
     telescope.load_extension("fzf")
     telescope.load_extension("telescope-tabs")
+    pcall(telescope.load_extension, "todo-comments") -- <- avoid crash on load
 
     local keymap = vim.keymap.set
     keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
@@ -27,6 +49,7 @@ return {
     keymap("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap("n", "<leader>ft", "<cmd>Telescope telescope-tabs list_tabs<cr>", { desc = "Find tabs" })
+    keymap("n", "<leader>fd", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
   cmd = { "Telescope" },
 }
