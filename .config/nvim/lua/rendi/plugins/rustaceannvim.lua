7ea1dd7d1e14423b138044e172a85ecdad5f9e24
rendi12345678 <hardleberg@gmail.com>
2025-07-12 15:06:14 +0700

    update

--- .config/nvim/lua/rendi/plugins/rustaceannvim.lua
+++ .config/nvim/lua/rendi/plugins/rustaceannvim.lua
@@ -1,65 +1,21 @@
-vim.g.rustaceanvim = {
-  server = {
-    settings = {
-      ["rust-analyzer"] = {
-        procMacro = {
-          enable = true,
-        },
-
-        cargo = {
-          allFeatures = true,
-          loadOutDirsFromCheck = true,
-          runBuildScripts = true,
-        },
-
-        checkOnSave = {
-          enable = false,
-        },
-
-        completion = {
-          autoimport = { enable = true },
-          postfix = { enable = true },
-        },
-
-        diagnostics = {
-          enable = true,
-          experimental = { enable = false },
-        },
-
-        inlayHints = { enable = false },
-        semanticHighlighting = { enable = false },
-        lens = { enable = false },
-
-        hover = {
-          actions = {
-            enable = false,
-          },
-        },
-
-        files = {
-          watcher = "client",
-        },
-      },
-    },
-
-    on_attach = function(client, bufnr)
-      if client.server_capabilities.semanticTokensProvider then
-        client.server_capabilities.semanticTokensProvider = nil
-      end
-
-      local map = function(keys, func)
-        vim.keymap.set("n", keys, func, { noremap = true, silent = true, buffer = bufnr })
-      end
-
-      map("<leader>ca", vim.lsp.buf.code_action)
-      map("gr", vim.lsp.buf.references)
-      map("K", vim.lsp.buf.hover)
-    end,
-  },
-}
-
 return {
   "mrcjkb/rustaceanvim",
   version = "^6",
   lazy = false,
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(_, bufnr)
+          local map = function(keys, cmd)
+            vim.keymap.set("n", keys, function()
+              vim.cmd.RustLsp(cmd)
+            end, { silent = true, buffer = bufnr })
+          end
+
+          map("<leader>ca", "codeAction")
+          map("K", { "hover", "actions" })
+        end,
+      },
+    }
+  end,
 }
