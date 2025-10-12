0503abb16dccfebd3773b9f35d4c2b24cdd4aa69
rendivs925 <hardleberg@gmail.com>
2025-10-12 12:21:56 +0800

    save

--- .config/nvim/lua/rendi/plugins/coq.lua
+++ .config/nvim/lua/rendi/plugins/coq.lua
@@ -21,14 +21,10 @@ return {
     }
   end,
   config = function()
-    local lsp = require("lspconfig")
     local coq = require("coq")
+    local lsp_util = require("lspconfig.util") -- needed for root_pattern
 
-    local function setup_lsp(server, opts)
-      opts = opts or {}
-      lsp[server].setup(coq.lsp_ensure_capabilities(opts))
-    end
-
+    -- Toggle diagnostics
     local function configure_diagnostics(enabled)
       vim.diagnostic.config({
         virtual_text = enabled,
@@ -37,118 +33,91 @@ return {
       })
     end
 
-    local function set_keybindings()
-      vim.keymap.set("n", "<leader>dh", function()
+    -- Keymaps for LSP and completion
+    local function on_attach(client, bufnr)
+      local function buf_map(mode, lhs, rhs, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, lhs, rhs, opts)
+      end
+
+      -- Toggle diagnostics
+      buf_map("n", "<leader>dh", function()
         configure_diagnostics(false)
       end, { desc = "Hide Diagnostics" })
-      vim.keymap.set("n", "<leader>ds", function()
+      buf_map("n", "<leader>ds", function()
         configure_diagnostics(true)
       end, { desc = "Show Diagnostics" })
 
-      vim.api.nvim_set_keymap(
-        "i",
-        "<Esc>",
-        [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]],
-        { expr = true, silent = true, desc = "Close/Cancel Completion" }
-      )
-      vim.api.nvim_set_keymap(
-        "i",
-        "<C-c>",
-        [[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]],
-        { expr = true, silent = true, desc = "Interrupt" }
-      )
-      vim.api.nvim_set_keymap(
-        "i",
-        "<BS>",
-        [[pumvisible() ? "\<C-e><BS>" : "\<BS>"]],
-        { expr = true, silent = true, desc = "Backspace" }
-      )
-      vim.api.nvim_set_keymap(
+      -- Completion keymaps
+      local expr_opts = { expr = true, silent = true }
+      buf_map("i", "<Esc>", [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]], expr_opts)
+      buf_map("i", "<C-c>", [[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]], expr_opts)
+      buf_map("i", "<BS>", [[pumvisible() ? "\<C-e><BS>" : "\<BS>"]], expr_opts)
+      buf_map(
         "i",
         "<Tab>",
         [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"]],
-        { expr = true, silent = true, desc = "Accept/Trigger Completion" }
+        expr_opts
       )
-      vim.api.nvim_set_keymap(
-        "i",
-        "<C-n>",
-        [[pumvisible() ? "\<C-n>" : "\<Tab>"]],
-        { expr = true, silent = true, desc = "Next Completion Item" }
-      )
-      vim.api.nvim_set_keymap(
-        "i",
-        "<C-p>",
-        [[pumvisible() ? "\<C-p>" : "\<BS>"]],
-        { expr = true, silent = true, desc = "Previous Completion Item" }
-      )
+      buf_map("i", "<C-n>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], expr_opts)
+      buf_map("i", "<C-p>", [[pumvisible() ? "\<C-p>" : "\<BS>"]], expr_opts)
     end
 
-    local function setup_lsp_servers()
-      setup_lsp("ts_ls", {
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-        root_dir = lsp.util.root_pattern("package.json", "tsconfig.json", ".git") or vim.fn.getcwd(),
-      })
-
-      setup_lsp("html", {
-        filetypes = { "html" },
-        root_dir = lsp.util.root_pattern(".git") or vim.fn.getcwd(),
-      })
-
-      setup_lsp("cssls", {
-        filetypes = { "css", "scss" },
-      })
-
-      -- setup_lsp("asm_lsp", {
-      --   filetypes = { "asm", "nasm" },
-      -- })
-
-      setup_lsp("tailwindcss", {
-        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescriptreact" },
-        root_dir = function(fname)
-          return lsp.util.root_pattern("tailwind.config.js", ".git")(fname) or vim.fn.getcwd()
-        end,
-      })
-
-      setup_lsp("lua_ls", {
-        filetypes = { "lua" },
-        settings = {
-          Lua = {
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      })
-
-      setup_lsp("graphql", {
-        filetypes = { "graphql", "gql" },
-      })
-
-      setup_lsp("emmet_ls", {
-        filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
-      })
-
-      -- setup_lsp("prismals", {
-      --   filetypes = { "prisma" },
-      -- })
-
-      -- setup_lsp("pyright", {
-      --   filetypes = { "python" },
-      --   settings = {
-      --     python = {
-      --       analysis = {
-      --         typeCheckingMode = "basic",
-      --       },
-      --     },
-      --   },
-      -- })
+    -- Helper to setup server with COQ capabilities
+    local function setup_server(server_name, opts)
+      opts = opts or {}
+      opts.on_attach = on_attach
+      opts.capabilities = coq.lsp_ensure_capabilities(vim.lsp.protocol.make_client_capabilities())
+      vim.lsp.config(server_name, opts)
+      vim.lsp.enable(server_name)
     end
 
     configure_diagnostics(true)
-    set_keybindings()
-    setup_lsp_servers()
+
+    -- === LSP servers ===
+    setup_server("tsserver", {
+      filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+      root_dir = lsp_util.root_pattern("package.json", "tsconfig.json", ".git"),
+    })
+
+    setup_server("html", {
+      filetypes = { "html" },
+      root_dir = lsp_util.root_pattern(".git"),
+    })
+
+    setup_server("cssls", {
+      filetypes = { "css", "scss" },
+    })
+
+    setup_server("tailwindcss", {
+      filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescriptreact" },
+      root_dir = function(fname)
+        return lsp_util.root_pattern("tailwind.config.js", ".git")(fname) or vim.fn.getcwd()
+      end,
+    })
+
+    setup_server("lua_ls", {
+      filetypes = { "lua" },
+      settings = {
+        Lua = {
+          workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+          telemetry = { enable = false },
+        },
+      },
+    })
+
+    setup_server("graphql", {
+      filetypes = { "graphql", "gql" },
+    })
+
+    setup_server("emmet_ls", {
+      filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact" },
+    })
+
+    -- Uncomment to enable other servers
+    -- setup_server("pyright", { filetypes = { "python" }, settings = { python = { analysis = { typeCheckingMode = "basic" } } } })
+    -- setup_server("prismals", { filetypes = { "prisma" } })
+    -- setup_server("asm_lsp", { filetypes = { "asm", "nasm" } })
   end,
 }
--- .config/nvim/lua/rendi/plugins/markdown-preview.lua
+++ .config/nvim/lua/rendi/plugins/markdown-preview.lua
@@ -1,15 +1,25 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
-    vim.cmd([[
-      function! OpenMarkdownPreview(url)
-        call jobstart(["firefox", a:url], {"detach": v:true})
-      endfunction
-      let g:mkdp_browserfunc = "OpenMarkdownPreview"
-    ]])
+
+    -- Chrome-specific settings
+    vim.g.mkdp_browser = "chrome"
+    vim.g.mkdp_browserfunc = "open"
+
+    -- Optional: Customize Chrome launch if needed
+    vim.g.mkdp_browser_path = "/usr/bin/google-chrome-stable" -- Linux
+    -- vim.g.mkdp_browser_path = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'  -- macOS
+    -- vim.g.mkdp_browser_path = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'  -- Windows
+
+    -- Other useful settings
+    vim.g.mkdp_theme = "dark" -- or 'light'
+    vim.g.mkdp_port = ""
+    vim.g.mkdp_page_title = "${name}"
   end,
   ft = { "markdown" },
 }
