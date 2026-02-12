49b79a73441530b627c1bf3d5e9623e5be4bfeca
rendivs925 <hardleberg@gmail.com>
2026-02-12 15:49:34 +0800

    use cool file browser

--- .config/i3/scripts/fzf-filebrowser.sh
+++ .config/i3/scripts/fzf-filebrowser.sh
@@ -1,28 +1,101 @@
 #!/bin/bash
 
 # Lazy loading file browser using fzf inside terminal
-# Features: fuzzy search, preview, edit in nvim, copy path
+# Cool Night theme with transparency
+# Dynamically centered on any screen
 
 tmpfile=$(mktemp)
 trap "rm -f $tmpfile" EXIT
 
-alacritty -e bash -c "
+# Get screen dimensions
+screen=$(xrandr | grep '*' | head -1 | awk '{print $1}')
+screen_width=$(echo $screen | cut -d'x' -f1)
+screen_height=$(echo $screen | cut -d'x' -f2)
+
+# Window dimensions
+win_width=1400
+win_height=900
+
+# Calculate center position
+pos_x=$(( (screen_width - win_width) / 2 ))
+pos_y=$(( (screen_height - win_height) / 2 ))
+
+# Create temporary alacritty config with transparency and window size
+alacritty_config=$(mktemp)
+cat > "$alacritty_config" << EOF
+[window]
+opacity = 0.85
+padding = { x = 10, y = 10 }
+dynamic_padding = true
+
+[colors.primary]
+background = '#011423'
+foreground = '#CBE0F0'
+
+[colors.cursor]
+text = "#011423"
+cursor = "#47FF9C"
+
+[colors.normal]
+black = "#214969"
+red = "#E52E2E"
+green = "#44FFB1"
+yellow = "#FFE073"
+blue = "#0FC5ED"
+magenta = "#a277ff"
+cyan = "#24EAF7"
+white = "#24EAF7"
+
+[colors.bright]
+black = "#214969"
+red = "#E52E2E"
+green = "#44FFB1"
+yellow = "#FFE073"
+blue = "#A277FF"
+magenta = "#a277ff"
+cyan = "#24EAF7"
+white = "#24EAF7"
+EOF
+
+alacritty --config-file "$alacritty_config" \
+    --class fzf-filebrowser \
+    --option window.dimensions.columns=160 \
+    --option window.dimensions.lines=40 \
+    -e bash -c "
+    export FZF_DEFAULT_OPTS=\"\
+        --color=bg:#011423,bg+:#0A2A3F,fg:#CBE0F0,fg+:#CBE0F0 \
+        --color=hl:#0FC5ED,hl+:#0FC5ED,info:#FFE073,prompt:#0FC5ED \
+        --color=pointer:#47FF9C,marker:#44FFB1,spinner:#24EAF7,header:#214969 \
+        --color=border:#0A2A3F,gutter:#011423,query:#CBE0F0 \
+        --border=rounded \
+        --prompt='  ' \
+        --pointer='>' \
+        --marker='✓' \
+        --header='' \
+        --height=100% \
+        --layout=reverse \
+        --info=inline-right \
+        --separator='─' \
+        --scrollbar='│'\"
+    
     selected=\$(find ~ -type f 2>/dev/null | \
-        fzf --preview 'bat --color=always --style=numbers --line-range :500 {}' \
-            --preview-window=right:50%:wrap \
-            --height=100% \
-            --layout=reverse \
-            --border=rounded \
-            --prompt='Files> ' \
-            --header='Enter: open | Ctrl-O: nvim | Ctrl-Y: copy | Ctrl-P: preview' \
-            --bind 'ctrl-o:execute(alacritty -e nvim {+})' \
+        fzf \
+            --preview 'bat --color=always --style=numbers --line-range :500 {}' \
+            --preview-window=right:50%:wrap:border-left \
+            --prompt='  ' \
+            --header='╭─ Enter:open  Ctrl-O:nvim  Ctrl-Y:copy  Ctrl-T:preview  Ctrl-N:down  Ctrl-P:up ─╮' \
+            --header-first \
+            --bind 'ctrl-n:down' \
+            --bind 'ctrl-p:up' \
+            --bind 'ctrl-o:execute(alacritty --config-file $alacritty_config -e nvim {+})' \
             --bind 'ctrl-y:execute-silent(echo -n {+} | xclip -selection clipboard)+abort' \
-            --bind 'ctrl-p:toggle-preview' \
-            --bind 'enter:accept' \
-            --color='fg:#a9b1d6,bg:#1a1b26,hl:#7aa2f7,fg+:#c0caf5,bg+:#24283b,hl+:#7aa2f7,info:#e0af68,prompt:#7aa2f7,pointer:#f7768e,marker:#9ece6a,spinner:#9ece6a,header:#565f89,border:#414868')
+            --bind 'ctrl-t:toggle-preview' \
+            --bind 'enter:accept')
     
     echo \"\$selected\" > $tmpfile
 "
+
+rm -f "$alacritty_config"
 
 selected=$(cat "$tmpfile")
 if [ -n "$selected" ]; then
