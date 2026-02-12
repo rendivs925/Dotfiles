3dd09758b33d95807b3a4ec61972cf5a7a9fe6de
rendivs925 <hardleberg@gmail.com>
2026-02-12 18:23:39 +0800

    update

--- .config/i3/scripts/fzf-filebrowser.sh
+++ .config/i3/scripts/fzf-filebrowser.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Lazy loading file browser using fzf inside Alacritty
-# Window size and position handled by i3 (NOT here)
+# FZF File Browser using Alacritty
+# Geometry handled by i3
+# Fast search powered by fd
 
 tmpfile=$(mktemp)
 trap "rm -f $tmpfile" EXIT
 
-# Temporary Alacritty theme config
 alacritty_config=$(mktemp)
 
 cat > "$alacritty_config" << 'EOF'
@@ -48,37 +48,49 @@ alacritty --config-file "$alacritty_config" \
     --class fzf-filebrowser \
     --title fzf-filebrowser \
     -e bash -c "
-    export FZF_DEFAULT_OPTS=\"\
-        --color=bg:#011423,bg+:#0A2A3F,fg:#CBE0F0,fg+:#CBE0F0 \
-        --color=hl:#0FC5ED,hl+:#0FC5ED,info:#FFE073,prompt:#0FC5ED \
-        --color=pointer:#47FF9C,marker:#44FFB1,spinner:#24EAF7,header:#214969 \
-        --color=border:#0A2A3F,gutter:#011423,query:#CBE0F0 \
-        --border=rounded \
+export FZF_DEFAULT_OPTS=\"
+    --color=bg:#011423,bg+:#0A2A3F,fg:#CBE0F0,fg+:#CBE0F0
+    --color=hl:#0FC5ED,hl+:#0FC5ED,info:#FFE073,prompt:#0FC5ED
+    --color=pointer:#47FF9C,marker:#44FFB1,spinner:#24EAF7,header:#214969
+    --color=border:#0A2A3F,gutter:#011423,query:#CBE0F0
+    --border=rounded
+    --prompt='  '
+    --pointer='>'
+    --marker='✓'
+    --header=''
+    --height=100%
+    --layout=reverse
+    --info=inline-right
+    --separator='─'
+    --scrollbar='│'
+\"
+
+# Use fd instead of find (fast + clean)
+selected=\$(fd --type f \
+    --hidden \
+    --follow \
+    --exclude .git \
+    --exclude node_modules \
+    --exclude .cache \
+    --exclude .local \
+    --exclude dist \
+    --exclude build \
+    --exclude target \
+    \$HOME 2>/dev/null | \
+    fzf \
+        --preview 'bat --color=always --style=numbers --line-range :500 {}' \
+        --preview-window=right:50%:wrap:border-left \
         --prompt='  ' \
-        --pointer='>' \
-        --marker='✓' \
-        --header='' \
-        --height=100% \
-        --layout=reverse \
-        --info=inline-right \
-        --separator='─' \
-        --scrollbar='│'\"
+        --header='╭─ Enter:open  Ctrl-O:nvim  Ctrl-Y:copy  Ctrl-T:preview  Ctrl-N:down  Ctrl-P:up ─╮' \
+        --header-first \
+        --bind 'ctrl-n:down' \
+        --bind 'ctrl-p:up' \
+        --bind 'ctrl-o:execute(alacritty -e nvim {+})' \
+        --bind 'ctrl-y:execute-silent(echo -n {+} | xclip -selection clipboard)+abort' \
+        --bind 'ctrl-t:toggle-preview' \
+        --bind 'enter:accept')
 
-    selected=\$(find ~ -type f 2>/dev/null | \
-        fzf \
-            --preview 'bat --color=always --style=numbers --line-range :500 {}' \
-            --preview-window=right:50%:wrap:border-left \
-            --prompt='  ' \
-            --header='╭─ Enter:open  Ctrl-O:nvim  Ctrl-Y:copy  Ctrl-T:preview  Ctrl-N:down  Ctrl-P:up ─╮' \
-            --header-first \
-            --bind 'ctrl-n:down' \
-            --bind 'ctrl-p:up' \
-            --bind 'ctrl-o:execute(alacritty -e nvim {+})' \
-            --bind 'ctrl-y:execute-silent(echo -n {+} | xclip -selection clipboard)+abort' \
-            --bind 'ctrl-t:toggle-preview' \
-            --bind 'enter:accept')
-
-    echo \"\$selected\" > $tmpfile
+echo \"\$selected\" > \"$tmpfile\"
 "
 
 rm -f "$alacritty_config"
