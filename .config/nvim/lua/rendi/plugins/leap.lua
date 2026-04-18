33aa608c15926ddc788915615aedb0303499cb06
rendivs925 <hardleberg@gmail.com>
2026-04-19 00:20:17 +0800

    update

--- .config/nvim/lua/rendi/plugins/leap.lua
+++ .config/nvim/lua/rendi/plugins/leap.lua
@@ -1,5 +1,5 @@
 return {
-  "ggandor/leap.nvim",
+  url = "https://codeberg.org/andyg/leap.nvim",
   event = "VeryLazy",
   config = function()
     local leap = require("leap")
--- .config/nvim/lua/rendi/plugins/telescope.lua
+++ .config/nvim/lua/rendi/plugins/telescope.lua
@@ -8,11 +8,19 @@ return {
     "nvim-tree/nvim-web-devicons",
     "LukasPietzschmann/telescope-tabs",
     "nvim-telescope/telescope-media-files.nvim",
+
+    {
+      "Marskey/telescope-sg",
+      dependencies = { "nvim-telescope/telescope.nvim" },
+    },
+
     {
       "folke/todo-comments.nvim",
       opts = {},
     },
+
     "nvim-telescope/telescope-file-browser.nvim",
+
     {
       "AckslD/nvim-neoclip.lua",
       dependencies = {
@@ -49,6 +57,7 @@ return {
       end,
     },
   },
+
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
@@ -57,7 +66,7 @@ return {
     local trouble_telescope = require("trouble.sources.telescope")
 
     local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
+      open_trouble_qflist = function()
         trouble.toggle("quickfix")
       end,
     })
@@ -73,37 +82,47 @@ return {
           },
         },
       },
+
       extensions = {
         media_files = {
-          -- filetypes whitelist
-          -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-          -- filetypes = { "png", "webp", "jpg", "jpeg" },
-          -- find command (defaults to `fd`)
           find_cmd = "rg",
+        },
+
+        ast_grep = {
+          command = { "sg", "--json=stream" },
         },
       },
     })
 
     telescope.load_extension("media_files")
-
     telescope.load_extension("fzf")
     telescope.load_extension("telescope-tabs")
-    pcall(telescope.load_extension, "todo-comments")
+    telescope.load_extension("file_browser")
     telescope.load_extension("neoclip")
+    telescope.load_extension("ast_grep")
+    pcall(telescope.load_extension, "todo-comments")
 
     local keymap = vim.keymap.set
+
     keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+
     keymap("n", "<space>fb", function()
       require("telescope").extensions.file_browser.file_browser()
-    end)
-    keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap("n", "<leader>ft", "<cmd>Telescope telescope-tabs list_tabs<cr>", { desc = "Find tabs" })
-    keymap("n", "<leader>fd", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    end, { desc = "File browser" })
+
+    keymap("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+    keymap("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Grep string" })
+    keymap("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Grep word under cursor" })
+
+    keymap("n", "<leader>fa", "<cmd>Telescope ast_grep<cr>", { desc = "AST Grep search" })
+
+    keymap("n", "<leader>ft", "<cmd>Telescope telescope-tabs list_tabs<cr>", { desc = "Tabs" })
+    keymap("n", "<leader>fd", "<cmd>TodoTelescope<cr>", { desc = "Todos" })
+
     keymap("n", "<leader>fy", function()
       require("telescope").extensions.neoclip.plus({ extra = '",star' })
-    end, { desc = "Clipboard history (system + unnamed + star)" })
+    end, { desc = "Clipboard history" })
   end,
+
   cmd = { "Telescope" },
 }
