ef37deb614d73045219f6c872a598769d56d5db8
rendivs925 <hardleberg@gmail.com>
2026-06-23 12:21:23 +0800

    lualine: relative path, file status (+/-), remove fugitive

--- .config/nvim/lua/rendi/plugins/lualine.lua
+++ .config/nvim/lua/rendi/plugins/lualine.lua
@@ -47,13 +47,15 @@ return {
     sections = {
       lualine_a = { { "mode", separator = { left = "" }, padding = { left = 1, right = 1 } } },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
+      lualine_c = { { "filename", path = 1, file_status = true } },
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
-      lualine_z = { { "location", separator = { right = "" }, padding = { left = 1, right = 1 } } },
+      lualine_z = {
+        { "location", separator = { right = "" }, padding = { left = 1, right = 1 } },
+      },
     },
     inactive_sections = {
-      lualine_c = { "filename" },
+      lualine_c = { { "filename", path = 1 } },
       lualine_x = { "location" },
     },
   },
