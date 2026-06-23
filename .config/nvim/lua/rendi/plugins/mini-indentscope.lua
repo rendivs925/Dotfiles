0b81ed622ab7209e5968a3519ce8da86a06f5500
rendivs925 <hardleberg@gmail.com>
2026-06-23 12:38:46 +0800

    indentscope: use fg_gutter color matching colorscheme

--- .config/nvim/lua/rendi/plugins/mini-indentscope.lua
+++ .config/nvim/lua/rendi/plugins/mini-indentscope.lua
@@ -7,5 +7,7 @@ return {
       symbol = "│",
       options = { try_as_border = true },
     })
+
+    vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#627E97", nocombine = true })
   end,
 }
