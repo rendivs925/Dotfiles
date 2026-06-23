3ca258fb9352d2fa24e8597cfd3377b39c1e6017
rendivs925 <hardleberg@gmail.com>
2026-06-23 12:27:36 +0800

    fix: correct rofi/polybar paths (remove nested duplicates)

--- .config/polybar/polybar/uptime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-output=$(uptime -p)
-output="${output/up/UP}"
-output="${output/up/UP}"
-echo "%{F#47FF9C}UP%{F-}${output#UP}"
--- .config/polybar/polybar/wifi-ssid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ssid=$(nmcli -t -f ACTIVE,SSID device wifi | grep "^yes" | cut -d: -f2)
-
-if [ -n "$ssid" ]; then
-    echo "%{F#47FF9C}wifi%{F-} $ssid"
-else
-    echo ""
-fi
