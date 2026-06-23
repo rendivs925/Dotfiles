bc581371cf258da29926eaef6dca6b63fb7bed3f
rendivs925 <hardleberg@gmail.com>
2026-06-23 11:38:01 +0800

    fix: lualine explicit tokyonight theme, remove unsupported globalstatus

--- .config/nvim/lua/rendi/plugins/lualine.lua
+++ .config/nvim/lua/rendi/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
-      theme = "auto",
+      theme = "tokyonight",
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
     },
