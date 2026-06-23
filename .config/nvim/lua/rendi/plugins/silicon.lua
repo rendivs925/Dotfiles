0100b050aeccbf206bc4c0d0c629c1966daa4c99
rendivs925 <hardleberg@gmail.com>
2026-06-23 14:59:46 +0800

    fix: silicon background uses palette.bg instead of hardcoded hex

--- .config/nvim/lua/rendi/plugins/silicon.lua
+++ .config/nvim/lua/rendi/plugins/silicon.lua
@@ -1,6 +1,5 @@
 return {
   "michaelrommel/nvim-silicon",
-  lazy = true,
   cmd = "Silicon",
   main = "nvim-silicon",
   keys = {
@@ -8,15 +7,18 @@ return {
     { "<leader>sf", function() require("nvim-silicon").file() end, desc = "Save code screenshot as file", mode = "v" },
     { "<leader>ss", function() require("nvim-silicon").shoot() end, desc = "Create code screenshot", mode = "v" },
   },
-  opts = {
-    line_offset = function(args)
-      return args.line1
-    end,
-    font = "Hack Nerd Font",
-    theme = "Coldark-Dark",
-    background = "#15202b",
-    shadow_blur_radius = 4,
-    shadow_offset_x = 2,
-    shadow_offset_y = 2,
-  },
+  config = function()
+    local c = require("rendi.core.palette")
+    require("nvim-silicon").setup({
+      line_offset = function(args)
+        return args.line1
+      end,
+      font = "Hack Nerd Font",
+      theme = "Coldark-Dark",
+      background = c.bg,
+      shadow_blur_radius = 4,
+      shadow_offset_x = 2,
+      shadow_offset_y = 2,
+    })
+  end,
 }
