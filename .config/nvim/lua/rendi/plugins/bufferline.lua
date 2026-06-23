f397f8322888a5ba1fd785118ed0b5cefd207d46
rendivs925 <hardleberg@gmail.com>
2026-06-23 11:32:12 +0800

    simplify: bufferline bare minimum

--- .config/nvim/lua/rendi/plugins/bufferline.lua
+++ .config/nvim/lua/rendi/plugins/bufferline.lua
@@ -1,20 +1,10 @@
 return {
   "akinsho/bufferline.nvim",
-  event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
   opts = {
     options = {
       mode = "tabs",
-      diagnostics = "nvim_lsp",
-      offsets = {
-        {
-          filetype = "NvimTree",
-          text = "File Explorer",
-          highlight = "Directory",
-          separator = true,
-        },
-      },
     },
   },
 }
